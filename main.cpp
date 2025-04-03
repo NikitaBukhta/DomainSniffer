658389147e68ae65a69efd61a62af2302c986e53
Mykyta Bukhta <nikita.bukhta.dev@gmail.com>
2025-04-03 16:08:14 +0300

    Removed unnessacery includes

--- main.cpp
+++ main.cpp
@@ -1,20 +1,4 @@
-// #include <pcap.h>
-// #include <winsock2.h>
-// #include <ws2tcpip.h>
-
-// #include <stdio.h>
-// #include <stdlib.h>
-
-// #include <algorithm>
-// #include <climits>
-// #include <iostream>
-// #include <string>
-// #include <thread>
-// #include <vector>
-
 #include "NetworkManager.hpp"
-
-#pragma comment(lib, "Ws2_32.lib")
 
 int main()
 {
